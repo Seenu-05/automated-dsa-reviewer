f8f92667b6e0463301e55e3571bc0fc272325532
Seenu <ch24btech11035@iith.ac.in>
2026-06-03 12:16:51 +0530

    Testing OpenAI brain with Brute-Force Two sum

--- test.cpp
+++ test.cpp
@@ -1,44 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <stack>
 
 using namespace std;
 
-vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-    unordered_map<int, int> mp;
-    stack<int> st;
-    
-    for(int i = nums2.size() - 1; i >= 0; i--) {
-        while(!st.empty() && nums2[i] >= st.top()) {
-            st.pop();
+vector<int> twoSum(vector<int>& nums, int target) {
+    // Nested loop checking every possible combination
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = i + 1; j < nums.size(); j++) {
+            if (nums[i] + nums[j] == target) {
+                return {i, j}; 
+            }
         }
-        mp[nums2[i]] = (st.empty()) ? -1 : st.top();
-        st.push(nums2[i]);
     }
-    
-    for(int i = 0; i < nums1.size(); i++) {
-        // Optional: If there's a chance nums1 has elements NOT in nums2, 
-        // you would handle it like this:
-        // nums1[i] = mp.count(nums1[i]) ? mp[nums1[i]] : -1;
-        
-        nums1[i] = mp[nums1[i]];
-    }
-    
-    return nums1;
+    return {};
 }
-// testing the use of github api.
 
-int main(){
-    // Replaced 9 with 17 so v1 is a strict subset of v2
-    vector<int> v1 = {2, 5, 17, 3}; 
-    vector<int> v2 = {1, 2, 5, 10, 17, 3, 8, 4};
+int main() {
+    vector<int> nums = {2, 7, 11, 15, 22, 4};
+    int target = 9;
     
-    vector<int> ans = nextGreaterElement(v1, v2);
+    vector<int> result = twoSum(nums, target);
     
-    for(int i = ans.size()-1; i<=0; i++){
-        cout << ans[i] << " ";
+    if (!result.empty()) {
+        cout << "Indices found: " << result[0] << " and " << result[1] << endl;
+    } else {
+        cout << "No valid pair found." << endl;
     }
     
-    return 0; // Return 0 for success
+    return 0;
 }
